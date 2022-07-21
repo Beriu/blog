4ddbd7e704fefa35c963584a3ceec3acfc393afd
Beriu <tiberiu@example.com>
2022-07-21 18:49:44 +0300

    non working variant

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vitepress';
+import { defineConfigWithTheme } from 'vitepress';
+import Theme from "./theme/index";
 
-export default defineConfig({
+export default defineConfigWithTheme<typeof Theme>({
 
     lang: 'en-US',
     title: 'VitePress',
     description: 'Vite & Vue powered static site generator.',
 
-    themeConfig: {
-        
-    }
+    themeConfig: Theme
 });
